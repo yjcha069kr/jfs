8107198c7f5c3f145d91415f3324a7ef2a282d95
yjcha069kr <yjcha069kr@gmail.com>
2025-11-21 18:00:55 +0900

    251121

--- js/scrollMotion.js
+++ js/scrollMotion.js
@@ -1,24 +1,33 @@
 $(window).scroll(function () {
-            //변수 선언
-            const windowHeight = $(window).height();
-            let windowScrollTop = $(window).scrollTop();
-            const speed = 1000, 
-                  easing = "easeOutExpo", 
-                  pos = 0, 
-                  offset = 0.5;
+        //변수 선언
+        const windowHeight = $(window).height();
+        let windowScrollTop = $(window).scrollTop();
+        const speed = 1000, 
+              easing = "easeOutExpo", 
+              pos = 0, 
+              offset = 2.5;
 
-            // 오른쪽 상단에 스크롤 양과 창 높이 값 출력
-            // console.log(windowScrollTop + " , " + windowHeight);
+        // 오른쪽 상단에 스크롤 양과 창 높이 값 출력
+        // console.log(windowScrollTop + " , " + windowHeight);
 
-            if (windowScrollTop > windowHeight / offset) {
-                //참일 경우
-                // i는 0, 1, 2, 3
-                for (let i = 0; i < 4; i++) {
-                    $(".motion").eq(i).stop().animate({ top: pos, opacity: 1 }, speed * (i + 1) / 2, easing);
-                }
-            } 
-            else {
-                //거짓일 경우
-                $(".motion").stop().animate({ top: pos + 600, opacity: 0 }, speed, easing);
+        if (windowScrollTop > windowHeight * offset) {
+            // 참일 경우
+            // i는 0, 1, 2, 3
+            for (let i = 0; i < 4; i++) {
+                $(".motion").eq(i).stop().animate({ top: pos, opacity: 1 }, speed * (i + 1) / 2, easing);
             }
-        });
+        } 
+        else {
+            // 거짓일 경우
+            $(".motion").stop().animate({ top: pos + 600, opacity: 0 }, speed, easing);
+        }
+
+        // '창의 스크롤 양이 0보다 크면'
+        if (windowScrollTop > 0) {
+            // '헤더 요소에 active 클래스 추가'
+            $("#header").addClass("active");
+        } else {
+            // '헤더 요소에 active 클래스 제거'
+            $("#header").removeClass("active");
+        }
+    }); /* .$(window).scroll(); */
