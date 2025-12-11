553750726a8bc98ceb4bfccd4b99e042409db89f
yjcha069kr <yjcha069kr@gmail.com>
2025-12-11 18:00:36 +0900

    251211

--- megabox/js/common.js
+++ megabox/js/common.js
@@ -1,10 +1,18 @@
 // header 2depth background
 // :not(:nth-of-type(5))
 // 스토어 메뉴는 '하위메뉴가 없으므로 제외'
-$('.main li:not(:nth-of-type(5)').mouseenter((e)=>{
+$('.main > li:not(:nth-of-type(5)').mouseenter(()=>{
     $('#header').addClass('active');
+    // $(this)는 마우스가 들어간 메인메뉴 li
+    $(this).find('#gnb .sub').addClass('active');
 });
-$('.main li').mouseleave(()=>{
+$('.main > li').mouseleave(()=>{
+    $('#header').removeClass('active');
+});
+$('#header .subbar').mouseenter(()=>{
+    $('#header').removeClass('active');
+});
+$('#header .subbar').mouseleave(()=>{
     $('#header').removeClass('active');
 });
 
